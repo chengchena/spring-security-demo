f34ea4251db103b72c56de2e18016d1e8c0d8b64
chencheng <823607596@qq.com>
2022-01-09 22:16:29 +0800

    只允许一个用户登陆，踢掉之前的用户

---
+++ one-user-login-1/src/main/java/com/chengchen/oneuserlogin1/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.chengchen.oneuserlogin1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "公众号：CoolCity ";
+    }
+}
---
+++ one-user-login-1/src/test/java/com/chengchen/oneuserlogin1/OneUserLogin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengchen.oneuserlogin1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneUserLogin1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
